8953a18992c5b222737c79be6852ce6a27ed137f
TruongNguyen <truong270901@gmail.com>
2024-03-01 11:03:58 +0700

    done memory FE

--- admin/views/Memory/edit.php
+++ admin/views/Memory/edit.php
@@ -4,22 +4,22 @@
     <main class="container mt-5 mb-5">
         <div class="row">
             <div class="col-sm">
-                <h3 class="text-center text-uppercase fw-bold">Sửa ram</h3>
-                <form action="index.php?controller=ram&action=update" method="post">
+                <h3 class="text-center text-uppercase fw-bold">Sửa Memory</h3>
+                <form action="index.php?controller=memory&action=update" method="post">
                     <div class="input-group mt-3 mb-3">
-                        <input type="text" class="form-control" required name="id" value="<?= $ram["id"] ?>" hidden>
+                        <input type="text" class="form-control" required name="id" value="<?= $memory["id"] ?>" hidden>
                     </div>
                     <div class="input-group mt-3 mb-3">
                         <span class="input-group-text">Name</span>
-                        <input type="text" class="form-control" required name="name" value="<?= $ram["name"] ?>">
+                        <input type="text" class="form-control" required name="name" value="<?= $memory["name"] ?>">
                     </div>
                     <div class="input-group mt-3 mb-3">
                         <span class="input-group-text">Value</span>
-                        <textarea type="text" class="form-control" name="value" required><?= $ram["value"] ?></textarea>
+                        <textarea type="text" class="form-control" name="value" required><?= $memory["value"] ?></textarea>
                     </div>
                     <div class="form-group  float-end ">
                         <input type="submit" value="Đồng ý" class="btn btn-success">
-                        <a href="index.php?controller=ram&action=index" class="ßtn btn-warning ">Quay lại</a>
+                        <a href="index.php?controller=memory&action=index" class="ßtn btn-warning ">Quay lại</a>
                     </div>
                 </form>
             </div>

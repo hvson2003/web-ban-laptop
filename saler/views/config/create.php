85cd2d47e6d86888cad72b8477051d3ce19a0e7b
DESKTOP-ETKC6SQ\truon <truong270901@gmail.com>
2024-03-04 23:27:43 +0700

    fix config BE

--- saler/views/config/create.php
+++ saler/views/config/create.php
@@ -1,11 +1,11 @@
-<?=template_header('Thêm order')?>
+<?=template_header('Thêm config')?>
 
 <body>
     <main class="container mt-5 mb-5">
         <div class="row">
             <div class="col-sm">
-                <h3 class="text-center text-uppercase fw-bold title_create">Thêm order</h3>
-                <form class="form_create" action="index.php?controller=order&action=store" method="post">
+                <h3 class="text-center text-uppercase fw-bold title_create">Thêm config</h3>
+                <form class="form_create" action="index.php?controller=config&action=store" method="post">
                     <div class="input-group mt-3 mb-3">
                         <span class="input-group-text">config_key</span>
                         <input type="text" class="form-control" value=1 required name="config_key">
@@ -20,7 +20,7 @@
                     </div>
                     <div class="form-group  float-end ">
                         <input type="submit" value="Thêm" class="btn btn-success">
-                        <a href="index.php?controller=order&action=index" class="btn btn-warning ">Quay lại</a>
+                        <a href="index.php?controller=config&action=index" class="btn btn-warning ">Quay lại</a>
                     </div>
                 </form>
             </div>
--- saler/views/config/edit.php
+++ saler/views/config/edit.php
@@ -1,29 +1,29 @@
-<?=template_header('Sửa order')?>
+<?=template_header('Sửa config')?>
 
 <body>
     <main class="container mt-5 mb-5">
         <div class="row">
             <div class="col-sm">
-                <h3 class="text-center text-uppercase fw-bold title_edit">Sửa order</h3>
-                <form class="form_edit" action="index.php?controller=order&action=update" method="post">
+                <h3 class="text-center text-uppercase fw-bold title_edit">Sửa config</h3>
+                <form class="form_edit" action="index.php?controller=config&action=update" method="post">
                     <div class="input-group mt-3 mb-3">
-                        <input type="text" class="form-control" required name="id" value="<?= $order["id"] ?>" hidden>
+                        <input type="text" class="form-control" required name="id" value="<?= $config["id"] ?>" hidden>
                     </div>
                     <div class="input-group mt-3 mb-3">
                         <span class="input-group-text">config_key</span>
-                        <input type="text" class="form-control" required name="config_key" value="<?= $order["config_key"] ?>">
+                        <input type="text" class="form-control" required name="config_key" value="<?= $config["config_key"] ?>">
                     </div>
                     <div class="input-group mt-3 mb-3">
                         <span class="input-group-text">config_value</span>
-                        <input type="text" class="form-control" required name="config_value" value="<?= $order["config_value"] ?>">
+                        <input type="text" class="form-control" required name="config_value" value="<?= $config["config_value"] ?>">
                     </div>
                     <div class="input-group mt-3 mb-3">
                         <span class="input-group-text">config_desc</span>
-                        <textarea type="text" class="form-control" name="config_desc" required><?= $order["config_desc"] ?></textarea>
+                        <textarea type="text" class="form-control" name="config_desc" required><?= $config["config_desc"] ?></textarea>
                     </div>
                     <div class="form-group  float-end ">
                         <input type="submit" value="Đồng ý" class="btn btn-success">
-                        <a href="index.php?controller=order&action=index" class="btn btn-warning ">Quay lại</a>
+                        <a href="index.php?controller=config&action=index" class="btn btn-warning ">Quay lại</a>
                     </div>
                 </form>
             </div>

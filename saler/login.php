68b091994444ec9e019396a849d162ad9300ed2a
hvsonn <hvson2003@gmail.com>
2024-03-15 19:22:22 +0700

    fix login saler

--- saler/login.php
+++ saler/login.php
@@ -11,19 +11,20 @@ if ( mysqli_connect_errno() ) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-if ($stmt = $con->prepare('SELECT id, password FROM users WHERE role=1 AND username = ?')) {
+if ($stmt = $con->prepare('SELECT id, password,role FROM users WHERE role=1 AND username = ?')) {
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
 	$stmt->store_result();
 
     if ($stmt->num_rows > 0) {
-        $stmt->bind_result($id, $password);
+        $stmt->bind_result($id, $password, $role);
         $stmt->fetch();
         // if (password_verify($_POST['password'], $password)) {
         if ($_POST['password'] == $password) {
             $_SESSION['logined'] = true;
             $_SESSION['name'] = $_POST['username'];
             $_SESSION['id'] = $id;
+            $_SESSION['role'] = $role;
             header('Location: index.php?action=index&controller=dashboard');
         } else {
             echo 'Incorrect password!';
